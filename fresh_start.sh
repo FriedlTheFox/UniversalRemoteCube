6e7d7e0031780ce7c9104d28768311b1d9927955
zwenson <zwenson@rocketmail.com>
2017-01-21 12:08:30 +0100

    Erste Version des Installationsskriptes, viele Anpassungen werden folgen

---
+++ fresh_start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash          
+#Installationsskript um eine arbeitsfähige 
+#Entwicklungsumgebung auf einem Raspberry Pi mit Raspian 
+#zur Verfügung zu stellen
+
+################################################################
+PACKAGESSYSTEM="git"
+PACKAGESPYTHON="python python-dev python-pylirc"
+PACKAGESMISC=""  
+
+PROJECTFOLDER="Projekte"
+GITHUBURL="https://github.com/FriedlTheFox/UniversalRemoteCube"
+################################################################
+
+
+echo "Aktualisiere Paketquellen"
+sudo apt-get update
+
+echo "Installiere benötigte Pakete"
+sudo apt-get install $PACKAGES $PACKAGESPYTHON $PACKAGESMISC
+
+
+echo "Projekt Ordner anlegen"
+cd $HOME
+mkdir -p $PROJECTFOLDER 
+cd ./$PROJECTFOLDER
+
+echo "Hole GitHub Projekt"
+git clone $GITHUBURL
